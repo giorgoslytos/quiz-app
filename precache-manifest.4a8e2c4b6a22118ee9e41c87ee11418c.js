353fd829abf3fa4d2d7644f290f8c89a6e97e479
giorgoslytos <giorgoslytos@gmail.com>
2020-08-09 21:27:46 +0300

    Updates

---
+++ precache-manifest.4a8e2c4b6a22118ee9e41c87ee11418c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f470c2a4415a3ea2b27bf2ca151319b1",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "10c006199dcf1401e198",
+    "url": "/quiz-app/static/css/main.896196f3.chunk.css"
+  },
+  {
+    "revision": "7bd81361398b5af175f6",
+    "url": "/quiz-app/static/js/2.0b83243f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quiz-app/static/js/2.0b83243f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10c006199dcf1401e198",
+    "url": "/quiz-app/static/js/main.fe9db781.chunk.js"
+  },
+  {
+    "revision": "cf0f19b536c77e8ae81a",
+    "url": "/quiz-app/static/js/runtime-main.af2ad936.js"
+  }
+]);
